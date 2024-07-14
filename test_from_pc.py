d1a63300df0493d96a425b2e289b951f0bd446db
Jivesh Kalra <jiveshkalra4@gmail.com>
2024-07-14 19:09:29 +0530

    Update test_from_pc.py

--- test_from_pc.py
+++ test_from_pc.py
@@ -7,10 +7,8 @@ print("running  ...")
 camera = cv2.VideoCapture(0)
 print("camera started ...")
 while True:
-    current_time = int(time.time())
-    print(current_time%5)
-    print(current_time)
-    if current_time % 5 == 0:
+    current_time = int(time.time()) 
+    if current_time % 3 == 0:
         return_value, image = camera.read()
         cv2.imwrite(f'pics/{current_time}.jpg', image)
         files = {'image': open(f'pics/{current_time}.jpg', 'rb')}
