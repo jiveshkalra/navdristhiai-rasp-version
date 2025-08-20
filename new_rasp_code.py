fd409c7bfb34652408903f8f554c927c12bc382e
Jivesh Kalra <127482713+jiveshkalra@users.noreply.github.com>
2025-08-20 09:22:24 +0530

    push

--- new_rasp_code.py
+++ new_rasp_code.py
@@ -126,7 +126,7 @@ def take_pic():
 def call_gemini_vlm(image_path, query, client):
     base64_image = encode_image(image_path)
     
-    model = "gemini-2.5-flash"
+    model = "gemini-2.0-flash"
     contents = [
         types.Content(
             role="user",
